f3d75de66d561c09b7ce29b568b83a03956197b8
mmegarba <micaeljohnmegarbane@gmail.com>
2017-11-29 14:20:04 -0500

    fix

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const cors = require("cors")
+const bodyParser = require("body-parser");
+
 var path = require("path");
 
 
